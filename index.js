f1a1c35c5bcc278c679250189d2b0b38ae118252
mward <mwardcoding@gmail.com>
2020-09-04 16:59:17 -0400

    added timeout and moved twitch auth function to my auth

--- index.js
+++ index.js
@@ -8,29 +8,14 @@ const { exec } = require('child_process')
 const app = express()
 const port = 3000
 
-auth.checkTokenFileExists();
-auth.generateNgrokUrl();
-const tokenInfo = JSON.parse(fs.readFileSync('token.json'));
-const ngrokInfo = JSON.parse(fs.readFileSync('ngrok.json'));
-const user_id = 568825210
+
+    auth.checkTokenFileExists()
+    auth.generateNgrokUrl()
+
+    setTimeout(() => {auth.getHooked()}, 1000);
 
 
-const body = {
-  'hub.callback': ngrokInfo,
-  'hub.mode': 'subscribe',
-  'hub.topic': `https://api.twitch.tv/helix/users/follows?first=1&from_id=${user_id}`,
-  'hub.lease_seconds': '360',
-  'hub.secret': process.env.HUB_SECRET 
-}
 
-fetch('https://api.twitch.tv/helix/webhooks/hub', {
-  method: 'POST',
-  body:   JSON.stringify(body),
-  headers: {'Client-ID': process.env.CLIENT_ID,
-            'Authorization': `Bearer ${tokenInfo.access_token}`,
-            'Content-Type': 'application/json'},
-}).then(console.log('Webhooked'))
-.catch(err => console.error(err));
 
 app.use(express.json());
 
--- myauth.js
+++ myauth.js
@@ -19,13 +19,12 @@ const authURL = url.format({
 
 async function checkTokenFileExists() {
     if (!fs.existsSync('token.json')) {
-        await generateAccessToken(authURL)
-            .catch(function (err){
-                console.log("err")
-            })
-        }
+        await generateAccessToken(authURL)        
+    }
+    else {
         const token_info = JSON.parse(fs.readFileSync('token.json'))
         return validateAccessToken(token_info);
+    }
 }
 
 function validateAccessToken(token_info) {
@@ -45,7 +44,7 @@ function generateAccessToken(url) {
     fetch(authURL, { method: 'POST'})
         .then(res => res.json())
         .then(json => {
-            fs.writeFile('token.json', JSON.stringify(json), (err) => {
+            fs.writeFileSync('token.json', JSON.stringify(json), (err) => {
                 if (err) throw err;
                 console.log("Success.");
             })
@@ -58,7 +57,7 @@ function generateNgrokUrl() {
     .then(json => json.tunnels.find(tunnel => tunnel.proto === 'https'))
     .then(secureTunnel => {
         console.log(secureTunnel.public_url);
-        fs.writeFile('ngrok.json', JSON.stringify(secureTunnel.public_url), (err) =>{
+        fs.writeFileSync('ngrok.json', JSON.stringify(secureTunnel.public_url), (err) =>{
             if (err) throw err;
         });
     })
@@ -69,16 +68,35 @@ function generateNgrokUrl() {
         console.error(err)
     })
 }
+
+function getHooked() {
+    const user_id = 505063151
+
+    const tokenInfo = JSON.parse(fs.readFileSync('token.json'));
+    const ngrokInfo = JSON.parse(fs.readFileSync('ngrok.json'));
+    
+
+    const body = {
+        'hub.callback': ngrokInfo,
+        'hub.mode': 'subscribe',
+        'hub.topic': `https://api.twitch.tv/helix/users/follows?first=1&to_id=${user_id}`,
+        'hub.lease_seconds': '86400',
+        'hub.secret': process.env.HUB_SECRET 
+    }
+
+    fetch('https://api.twitch.tv/helix/webhooks/hub', {
+    method: 'POST',
+    body:   JSON.stringify(body),
+    headers: {'Client-ID': process.env.CLIENT_ID,
+                'Authorization': `Bearer ${tokenInfo.access_token}`,
+                'Content-Type': 'application/json'},
+    }).then(console.log('Webhooked'))
+    .catch(err => console.error(err));
+
+}
+
+
 exports.checkTokenFileExists = checkTokenFileExists;
 exports.generateNgrokUrl = generateNgrokUrl;
+exports.getHooked = getHooked;
 
-/*
-function checkStatus(res) {
-	if (res.ok) {
-		// res.status >= 200 && res.status < 300
-		return res;
-	} else {
-		throw MyCustomError(res.statusText);
-	}
-}
-*/
